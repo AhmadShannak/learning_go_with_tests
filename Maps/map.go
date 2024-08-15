57b8e0d5f2c1bc2f67987ebd7a19b32428d830d5
AhmadShannak <shannak.ahmad96@gmail.com>
2024-08-15 23:26:12 +0300

    maps p2

--- Maps/map.go
+++ Maps/map.go
@@ -1,20 +1,28 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryError string
 
-var NotFoundError = errors.New("Could not find the word in the given dictionary")
+const (
+	NotFoundError     = DictionaryError("Could not find the word in the given dictionary")
+	AlreadyExistError = DictionaryError("Could not add the element because it already exist")
+	None              = DictionaryError("")
+)
 
-func (d Dictionary) Search(key string) (string, error) {
+func (d Dictionary) Search(key string) (string, DictionaryError) {
 	value, found := d[key]
 
 	if !found {
 		return "", NotFoundError
 	}
-	return value, nil
+	return value, None
 }
 
-func (d Dictionary) Add(key, value string) {
+func (d Dictionary) Add(key, value string) DictionaryError {
+	_, error := d.Search(key)
+	if error == None {
+		return AlreadyExistError
+	}
 	d[key] = value
+	return None
 }
--- Maps/maps_test.go
+++ Maps/maps_test.go
@@ -14,7 +14,7 @@ func TestSearch(t *testing.T) {
 		d := Dictionary{"test": "this is just a test"}
 		_, er := d.Search("meow")
 		want := NotFoundError
-		if er == nil {
+		if er == None {
 			t.Fatal("expected to get error")
 		}
 		AssertError(t, er, want)
@@ -22,12 +22,22 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	d := Dictionary{"test": "this is just a test"}
-	d.Add("test2", "this is also another test")
+	t.Run("Adding New", func(t *testing.T) {
+		d := Dictionary{"test": "this is just a test"}
+		d.Add("test2", "this is also another test")
 
-	value, _ := d.Search("test2")
-	want := "this is also another test"
-	AssertString(t, value, want)
+		value, _ := d.Search("test2")
+		want := "this is also another test"
+		AssertString(t, value, want)
+	})
+
+	t.Run("Adding what already exist", func(t *testing.T) {
+		d := Dictionary{"test": "this is just a test"}
+		er := d.Add("test", "this is just a test")
+
+		want := AlreadyExistError
+		AssertError(t, er, want)
+	})
 }
 
 func AssertString(t *testing.T, got, want string) {
@@ -37,7 +47,7 @@ func AssertString(t *testing.T, got, want string) {
 	}
 }
 
-func AssertError(t *testing.T, got, want error) {
+func AssertError(t *testing.T, got, want DictionaryError) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
